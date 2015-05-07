f55ec6bc90c262ccfa84470b4dc210122b0a5d54
Victor Yang <yanky2000@yahoo.com>
2015-05-07 22:26:19 +0300

    real project code added

--- post2test/ViewController.swift
+++ post2test/ViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameField: UITextField!
+    @IBOutlet weak var enterMessageField: UITextField!
+    @IBOutlet weak var sendMessageButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +28,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendMessageButtonPressed(sender: UIButton) {
+        
+
+        nameLabel.text = enterNameField.text
+        messageLabel.text = enterMessageField.text
+        nameLabel.textColor = UIColor.brownColor()
+        messageLabel.textColor = UIColor.brownColor()
+        
+        
+        enterNameField.text = ""
+        enterMessageField.text = ""
+        
+        // Show name and message text on screen
+        nameLabel.hidden = false
+        messageLabel.hidden = false
+        
+        sendMessageButton.setTitle("Message sent", forState: UIControlState.Normal)
+        
+        //Hide keyboard
+        enterNameField.resignFirstResponder()
+        enterMessageField.resignFirstResponder()
+        
+        
+    }
 
 }
 
