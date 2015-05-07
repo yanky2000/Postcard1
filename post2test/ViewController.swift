d32200071f7645434397b0a3645eb2fe1680786c
Victor Yang <yanky2000@yahoo.com>
2015-05-07 20:56:13 +0300

    my first rev of project

---
+++ post2test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  post2test
+//
+//  Created by Victor Yang on 07.05.15.
+//  Copyright (c) 2015 Victor Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ post2testTests/post2testTests.swift
@@ -0,0 +1,36 @@
+//
+//  post2testTests.swift
+//  post2testTests
+//
+//  Created by Victor Yang on 07.05.15.
+//  Copyright (c) 2015 Victor Yang. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class post2testTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
